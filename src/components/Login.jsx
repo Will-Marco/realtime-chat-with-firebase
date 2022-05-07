abb34682e5af289fe0a2e736f3d174f6589b2356
Will-Marco <mr.marco0101@gmail.com>
2022-05-07 19:31:45 +0500

    The login page is stylized

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,20 @@
+import { Box, Button, Container, Grid } from "@mui/material";
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Container>
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: window.innerHeight - 50 }}
+      >
+        <Box>
+          <Button variant="outlined">Login with Google</Button>
+        </Box>
+      </Grid>
+    </Container>
+  );
 }
 
 export default Login;
